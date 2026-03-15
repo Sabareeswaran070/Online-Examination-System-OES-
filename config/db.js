64c22e61498fca0db24e496dfcf061ed03ffaf54
Sabareeswaran070 <sabareeswaran265@gmail.com>
2026-03-15 07:36:03 +0530

    Changes

--- config/db.js
+++ config/db.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const logger = require('./logger');
 
-const connectDB = async () => {
+const connectDB = async (retryCount = 0) => {
+  const MAX_RETRIES = 3;
+  const RETRY_INTERVAL = 5000;
+
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      family: 4 // Force IPv4 to resolve SRV issues on some networks
+    });
 
     logger.info(`MongoDB Connected: ${conn.connection.host}`);
     
@@ -13,12 +18,19 @@ const connectDB = async () => {
     });
 
     mongoose.connection.on('error', (err) => {
-      logger.error('MongoDB connection error:', err);
+      logger.error('MongoDB connection error: %O', err);
     });
 
   } catch (error) {
-    logger.error('Error connecting to MongoDB:', error.message);
-    process.exit(1);
+    logger.error('Error connecting to MongoDB: %s', error.message);
+    
+    if (retryCount < MAX_RETRIES) {
+      logger.info(`Retrying connection in ${RETRY_INTERVAL/1000}s... (Attempt ${retryCount + 1}/${MAX_RETRIES})`);
+      setTimeout(() => connectDB(retryCount + 1), RETRY_INTERVAL);
+    } else {
+      logger.error('Max retries reached. Exiting...');
+      process.exit(1);
+    }
   }
 };
 
